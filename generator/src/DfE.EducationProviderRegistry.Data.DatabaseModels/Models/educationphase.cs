09cdc326104aa780551403c6d138379dc550e4f5
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-24 09:27:24 +0100

    initial version

---
+++ generator/src/DfE.EducationProviderRegistry.Data.DatabaseModels/Models/educationphase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
+
+public partial class Educationphase
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Establishment> Establishment { get; set; } = new List<Establishment>();
+}
---
+++ generator/src/DfE.EducationProviderRegistry.Data.DatabaseModels/Models/establishmentgroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
+
+public partial class Establishmentgroup
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string TypeCode { get; set; } = null!;
+
+    public virtual Establishmentgrouptype TypeCodeNavigation { get; set; } = null!;
+
+    public virtual ICollection<Establishment> Urn { get; set; } = new List<Establishment>();
+}
